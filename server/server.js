53145109a447c4e3cde63790d88be5db19cc92f2
Ozair Ahmed <ozairboss2005@gmail.com>
2026-03-09 00:40:04 -0400

    See if you can get server.js working idk

--- server/server.js
+++ server/server.js
@@ -1,30 +1,34 @@
 const express = require('express')
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 const cors = require('cors')
-require('dotenv').config()
+// require('dotenv').config()
 
 const app = express()
 
 // Middleware
-app.use(cors())
-app.use(express.json())
+// app.use(cors())
+// app.use(express.json())
 
 // Test route
 app.get('/', (req, res) => {
   res.json({ message: 'MERN server is running!' })
 })
 
-// MongoDB connection
-const PORT = process.env.PORT || 5000
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log('MongoDB connected')
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`)
-    })
-  })
-  .catch(err => console.log(err))
+app.listen(5000, () => {
+    console.log("Server running on 5000 i think lol");
+})
+
+// // MongoDB connection
+// const PORT = process.env.PORT || 5000
+// mongoose
+//   .connect(process.env.MONGO_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log('MongoDB connected')
+//     app.listen(PORT, () => {
+//       console.log(`Server running on port ${PORT}`)
+//     })
+//   })
+//   .catch(err => console.log(err))
