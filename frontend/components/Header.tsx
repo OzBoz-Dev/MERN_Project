289a56b97531c02775ca2b300d73cd4b125b8e20
HML786 <haanilalji2@gmail.com>
2026-03-26 16:47:45 -0400

    Modify Header component to navigate to proper routes on tab click

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -3,33 +3,49 @@
 import styles from "./Header.module.css";
 import { Flex, Tabs, Text } from "@mantine/core";
 import { IconCode, IconHome, IconMessage } from "@tabler/icons-react";
+import { usePathname, useRouter } from "next/navigation";
 
 export function Header() {
+  const pathname = usePathname(); // Pathname of the current page
+  const router = useRouter();
+
   // For tab icons
   const iconSize = 18;
 
-  // Tab items w/ name and icon for easy editing
+  // Tab items w/ name, icon, and path for easy editing
   const tabItems = [
     {
       name: "Feed",
       icon: IconHome,
+      path: "/feed",
     },
     {
       name: "My Projects",
       icon: IconCode,
+      path: "/my-projects",
     },
     {
       name: "Messages",
       icon: IconMessage,
+      path: "/messages",
     },
   ];
 
+  // Determine the current active tab
+  // If pathname matches with a tabItem's "path", then this is the active tab
+  const activeTab =
+    tabItems.find((item) => item.path === pathname)?.name || null;
+
+  // Create a Tab for each tabItem
   const tabs = tabItems.map((tabItem) => {
     const Icon = tabItem.icon;
     return (
       <Tabs.Tab
         key={tabItem.name}
         value={tabItem.name}
+        onClick={() => {
+          router.push(tabItem.path); // navigate to defined path
+        }}
         leftSection={<Icon size={iconSize} />}
       >
         {tabItem.name}
@@ -50,6 +66,7 @@ export function Header() {
         <Tabs
           classNames={{ tab: styles.tab }}
           defaultValue={tabItems[0].name}
+          value={activeTab} // use active tab for the current tab value
           variant="unstyled"
         >
           <Tabs.List>{tabs}</Tabs.List>
