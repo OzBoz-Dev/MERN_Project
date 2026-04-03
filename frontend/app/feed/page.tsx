151b636d81315cb7de10a7acf95ec8447ec51a01
HML786 <haanilalji2@gmail.com>
2026-04-03 15:18:13 -0400

    Format with Prettier

--- frontend/app/feed/page.tsx
+++ frontend/app/feed/page.tsx
@@ -1,25 +1,29 @@
+import CommentCard from "@/components/CommentCard";
 import ProjectCard from "@/components/ProjectCard";
 
 export default function Home() {
   return (
-    <div style={{
-      width: "100%",
-      maxWidth: "900px",
-      padding: "0 16px"
-    }}>
+    <div
+      style={{
+        width: "100%",
+        maxWidth: "900px",
+        padding: "0 16px",
+      }}
+    >
       <ProjectCard
-      postTitle='title' 
-      user='del0m_' 
-      postTags={['ML developer', 'DevOps']}
-      description='the quick brown fox jumped over the lazy dog'
-      timeAgo='2 hours ago'/>
-      <ProjectCard 
-      postTitle='title' 
-      user='del0m_' 
-      postTags={['ML developer', 'DevOps']}
-      description='the quick brown fox jumped over the lazy dog'
-      timeAgo='2 hours ago'/>
+        postTitle="title"
+        user="del0m_"
+        postTags={["ML developer", "DevOps"]}
+        description="the quick brown fox jumped over the lazy dog"
+        timeAgo="2 hours ago"
+      />
+      <ProjectCard
+        postTitle="title"
+        user="del0m_"
+        postTags={["ML developer", "DevOps"]}
+        description="the quick brown fox jumped over the lazy dog"
+        timeAgo="2 hours ago"
+      />
     </div>
-    
   );
 }
