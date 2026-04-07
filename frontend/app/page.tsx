9ff183f7da5376f13ecd44937f75f62fd9971c43
HML786 <haanilalji2@gmail.com>
2026-04-07 01:52:03 -0400

    Remove API_ENTRYPOINT as it was moved to constants/constants.ts

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,21 +1,18 @@
-'use client'
- 
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
+"use client";
 
-export default function globalRedirect(){
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function globalRedirect() {
   const router = useRouter();
 
   useEffect(() => {
-    const token = localStorage.getItem('token');
-    if (token){
-      router.push('/feed')
+    const token = localStorage.getItem("token");
+    if (token) {
+      router.push("/feed");
+    } else {
+      router.push("/auth");
     }
-    else{
-      router.push('/auth')
-    }
-  }, [router])
-  return null
+  }, [router]);
+  return null;
 }
-
-export const API_ENTRYPOINT = "http://localhost:5000"
