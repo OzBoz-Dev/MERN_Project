af5da7452cdcf34b9c1d731954fc70a4bfdacf10
HML786 <haanilalji2@gmail.com>
2026-04-07 10:45:50 -0400

    Modify feed page and create new pages for nav bar

--- mern_mobile_app/lib/pages/feed/feed_page.dart
+++ mern_mobile_app/lib/pages/feed/feed_page.dart
@@ -3,33 +3,30 @@ import 'package:mern_mobile_app/models/tag.dart';
 import 'package:mern_mobile_app/widgets/project_card.dart';
 
 class FeedPage extends StatelessWidget {
-  const FeedPage({super.key});
+  FeedPage({super.key});
+
+  // Mock data
+  final mockPosts = List.filled(20, ProjectCard(
+    title: "Project Title",
+    poster: "hml786",
+    numLikes: 1000,
+    description: "The quick brown fox jumped over the lazy dog.",
+    tags: [
+      Tag(label: "ML Developer"),
+      Tag(label: "Frontend Developer"),
+      Tag(label: "DevOps"),
+    ],
+    dateTimePosted: DateTime.now().add(Duration(hours: -2)),
+  ));
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Feed"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
-        child: Column(
-          children: [
-            ProjectCard(
-              title: "Project Title",
-              poster: "hml786",
-              numLikes: 1000,
-              description: "The quick brown fox jumped over the lazy dog.",
-              tags: [
-                Tag(label: "ML Developer"),
-                Tag(label: "Frontend Developer"),
-                Tag(label: "DevOps"),
-              ],
-              dateTimePosted: DateTime.now().add(Duration(hours: -2)),
-            ),
-          ]
-        ),
-      ),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+      child: ListView.builder(
+        itemCount: mockPosts.length,
+        itemBuilder: (context, index) => mockPosts[index],
+      )
     );
   }
 }
---
+++ mern_mobile_app/lib/pages/my-projects/my_projects_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MyProjectsPage extends StatelessWidget {
+  const MyProjectsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("My Projects Page"),
+    );
+  }
+}
