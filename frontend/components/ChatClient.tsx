05480203147c89863306177d2ec6b53f7a20e792
Ozair Ahmed <ozairboss2005@gmail.com>
2026-04-21 01:43:09 -0400

    Put socket inside the function so it recieves env variables properly

--- frontend/components/ChatClient.tsx
+++ frontend/components/ChatClient.tsx
@@ -6,23 +6,22 @@ import { API_SERVER_ENTRYPOINT } from "@/constants/constants";
 import { Button, ScrollArea } from "@mantine/core";
 import { getCookie } from "cookies-next/client";
 import { useParams } from "next/navigation";
-import { useEffect, useState } from "react";
-import io from "socket.io-client";
+import { useEffect, useRef, useState } from "react";
+import io, { Socket } from "socket.io-client";
 import ChatInput from "@/components/ChatInput";
-
-const socket = io(API_SERVER_ENTRYPOINT);
 
 export default function ChatClient() {
     const { id } = useParams();
     const userId = getCookie("username");
+    const socketRef = useRef<Socket | null>(null);
 
     const [messages, setMessages] = useState<Message[]>([]);
 
     useEffect(() => {
         async function fetchConversation() {
-        const res = await fetch(API_SERVER_ENTRYPOINT + '/conversations/' + id + '/');
-        const data = await res.json();
-        setMessages(data.messages || []);
+            const res = await fetch(API_SERVER_ENTRYPOINT + '/conversations/' + id + '/');
+            const data = await res.json();
+            setMessages(data.messages || []);
         }
         if (id) fetchConversation();
     }, [id]);
@@ -30,6 +29,11 @@ export default function ChatClient() {
     // connect to client
     useEffect(() => {
         if(!id) return;
+        // Initialize socket inside the effect, client-side only
+        socketRef.current = io(API_SERVER_ENTRYPOINT);
+        const socket = socketRef.current;
+
+
         socket.emit("joinConversation", id);
 
         socket.on("newMessage", (message) => {
@@ -39,6 +43,7 @@ export default function ChatClient() {
         // clean up
         return () => {
             socket.off("newMessage");
+            socket.disconnect();
         };
     }, [id]);
 
