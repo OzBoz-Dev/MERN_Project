7ea9f2b9eb90e2df38d15cb3cb4ea3707cb6d79c
HML786 <haanilalji2@gmail.com>
2026-04-22 00:18:00 -0400

    Add button to create new conversation

--- mern_mobile_app/lib/pages/messages/create_conversation_page.dart
+++ mern_mobile_app/lib/pages/messages/create_conversation_page.dart
@@ -1,9 +1,14 @@
 import 'package:chip_in/models/user.dart';
+import 'package:chip_in/providers/auth_provider.dart';
+import 'package:chip_in/services/messages_service.dart';
 import 'package:chip_in/services/profile_service.dart';
 import 'package:chip_in/widgets/animated_grid_background.dart';
 import 'package:chip_in/widgets/user_holder.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_tabler_icons/flutter_tabler_icons.dart';
 import 'package:flutter_typeahead/flutter_typeahead.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
 class CreateConversationPage extends StatefulWidget {
   const CreateConversationPage({super.key});
@@ -29,16 +34,22 @@ class _CreateConversationPageState extends State<CreateConversationPage> {
               padding: const EdgeInsets.all(8.0),
               child: Card(
                 child: Padding(
-                  padding: const EdgeInsets.all(24),
+                  padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 32),
                   child: Column(
                     mainAxisSize: MainAxisSize.min,
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
                       Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text("New Conversation")
+                        alignment: Alignment.center,
+                        child: Text(
+                          "New Conversation",
+                          style: GoogleFonts.montserrat(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18
+                          ),
+                        )
                       ),
-                      const SizedBox(height: 12,),
+                      const SizedBox(height: 24,),
                       TypeAheadField<User>(
                         itemBuilder: (context, User user) {
                           return ListTile(
@@ -47,8 +58,18 @@ class _CreateConversationPageState extends State<CreateConversationPage> {
                         },
                         emptyBuilder: (context) => ListTile(title: Text("No users found!"),),
                         onSelected: (user) {
+                          // Get own username
+                          final authProvider = context.read<AuthProvider>();
                           final alreadyExists = _users.any((u) => u.username == user.username);
-                          if(alreadyExists) {
+                          // Tell user they can't add themselves
+                          if(user.username == authProvider.username) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text("Can't add yourself to a new conversation"),
+                              )
+                            );
+                          }
+                          else if(alreadyExists) {
                             ScaffoldMessenger.of(context).showSnackBar(
                               SnackBar(
                                 content: Text("You've already added this user!"),
@@ -90,6 +111,39 @@ class _CreateConversationPageState extends State<CreateConversationPage> {
                             _users.remove(deletedTag);
                           });
                         },
+                      ),
+                      const SizedBox(height: 24,),
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton.icon(
+                          onPressed: _users.isNotEmpty ? () async {
+                            final authProvider = context.read<AuthProvider>();
+                            final messagesService = MessagesService();
+
+                            final usernames = _users.map((user) => user.username).toList();
+
+                            // Add self to List
+                            usernames.add(authProvider.username!);
+
+                            try {
+                              // Create convo
+                              await messagesService.createConversation(authProvider.token!, usernames);
+                              // Go back to messages page
+                              if(mounted) Navigator.of(context).pop();
+                            }
+                            catch(e) {
+                              if(mounted) {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                    content: Text(e.toString()),
+                                  )
+                                );
+                              }
+                            }
+                          } : null,
+                          icon: Icon(TablerIcons.message_plus),
+                          label: Text("Create", style: GoogleFonts.montserrat(fontWeight: FontWeight.bold),),
+                        ),
                       ),
                     ]
                   ),
