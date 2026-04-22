83f2fe3683f8a80343ef0ddb98d1e2021a1487c0
HML786 <haanilalji2@gmail.com>
2026-04-22 00:23:30 -0400

    Use animated grid background for every case on MessagesPage

--- mern_mobile_app/lib/pages/messages/messages_page.dart
+++ mern_mobile_app/lib/pages/messages/messages_page.dart
@@ -34,95 +34,95 @@ class _MessagesPageState extends State<MessagesPage> {
     final currentUsername = authProvider.username ?? '';
 
     return Scaffold(
-      body: Consumer<ConversationProvider>(
-        builder: (context, conversationProvider, child) {
-          List<Conversation> conversations = conversationProvider.conversations;
-          // Loading and no convos - avoid flicker
-          if(conversationProvider.isLoading || !conversationProvider.hasLoaded) {
-            return const Center(child: CircularProgressIndicator());
-          }
-          else if(conversationProvider.error != null) {
-            return Center(
-              child: Text(
-                conversationProvider.error!,
-                textAlign: TextAlign.center,
-              ),
-            );
-          }
-          // Done loading
-          else {
-            // Convos still emtpy - we really don't have any
-            if(conversations.isEmpty) {
-             return AnimatedGridBackground(
-                backgroundColor: const Color(0xFFFDF8EA),
-                child: Padding(
-                  padding: const EdgeInsets.all(14),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Icon(
-                        TablerIcons.message_circle_question,
-                        color: Color(0xFFFFA500),
-                        size: 48,
-                      ),
-                      const SizedBox(height: 12,),
-                      Text(
-                        textAlign: TextAlign.center,
-                        "You have no messages.",
-                        style: GoogleFonts.montserrat(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w500
-                        ),
-                      ),
-                      const SizedBox(height: 36,),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 16),
-                        child: SizedBox(
-                          width: double.infinity,
-                          child: ElevatedButton.icon(
-                            onPressed: () {
-                              final conversationProvider = context.read<ConversationProvider>();
-                              Navigator.of(context).push(
-                                MaterialPageRoute(
-                                  builder: (context) => CreateConversationPage(),
-                                )
-                              ).then((_) => conversationProvider.getConversations());
-                            },
-                            icon: Icon(TablerIcons.message_plus),
-                            label: Text("Create Conversation", style: GoogleFonts.montserrat(fontWeight: FontWeight.bold),),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+      body: AnimatedGridBackground(
+        backgroundColor: const Color(0xFFFDF8EA),
+        child: Consumer<ConversationProvider>(
+          builder: (context, conversationProvider, child) {
+            List<Conversation> conversations = conversationProvider.conversations;
+            // Loading and no convos - avoid flicker
+            if(conversationProvider.isLoading || !conversationProvider.hasLoaded) {
+              return const Center(child: CircularProgressIndicator());
+            }
+            else if(conversationProvider.error != null) {
+              return Center(
+                child: Text(
+                  conversationProvider.error!,
+                  textAlign: TextAlign.center,
                 ),
               );
             }
-            // We do have conversations
+            // Done loading
             else {
-              return ListView.builder(
-                padding: const EdgeInsets.all(16),
-                itemCount: conversations.length,
-                itemBuilder: (context, index) {
-                  final convo = conversations[index];
-                  return ConversationCard(
-                    conversation: convo,
-                    currentUsername: currentUsername,
-                    onTap: () {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (_) => ChatPage(
-                            conversationId: convo.id,
+              // Convos still emtpy - we really don't have any
+              if(conversations.isEmpty) {
+               return Padding(
+                 padding: const EdgeInsets.all(14),
+                 child: Column(
+                   mainAxisAlignment: MainAxisAlignment.center,
+                   children: [
+                     const Icon(
+                       TablerIcons.message_circle_question,
+                       color: Color(0xFFFFA500),
+                       size: 48,
+                     ),
+                     const SizedBox(height: 12,),
+                     Text(
+                       textAlign: TextAlign.center,
+                       "You have no messages.",
+                       style: GoogleFonts.montserrat(
+                         fontSize: 18,
+                         fontWeight: FontWeight.w500
+                       ),
+                     ),
+                     const SizedBox(height: 36,),
+                     Padding(
+                       padding: const EdgeInsets.symmetric(horizontal: 16),
+                       child: SizedBox(
+                         width: double.infinity,
+                         child: ElevatedButton.icon(
+                           onPressed: () {
+                             final conversationProvider = context.read<ConversationProvider>();
+                             Navigator.of(context).push(
+                               MaterialPageRoute(
+                                 builder: (context) => CreateConversationPage(),
+                               )
+                             ).then((_) => conversationProvider.getConversations());
+                           },
+                           icon: Icon(TablerIcons.message_plus),
+                           label: Text("Create Conversation", style: GoogleFonts.montserrat(fontWeight: FontWeight.bold),),
+                         ),
+                       ),
+                     ),
+                   ],
+                 ),
+               );
+              }
+              // We do have conversations
+              else {
+                return ListView.builder(
+                  padding: const EdgeInsets.all(16),
+                  itemCount: conversations.length,
+                  itemBuilder: (context, index) {
+                    final convo = conversations[index];
+                    return ConversationCard(
+                      conversation: convo,
+                      currentUsername: currentUsername,
+                      onTap: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (_) => ChatPage(
+                              conversationId: convo.id,
+                            ),
                           ),
-                        ),
-                      );
-                    },
-                  );
-                },
-              );
+                        );
+                      },
+                    );
+                  },
+                );
+              }
             }
-          }
-        },
+          },
+        ),
       )
     );
   }
